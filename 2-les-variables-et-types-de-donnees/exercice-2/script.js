ca1161b2e037fd15bf4642b3465e6e2b38adc95c
David Vauzelle <dvauzelle@deastanceservices.fr>
2024-03-06 14:45:24 +0100

    Modification de l'exercice-2 du chapitre 2

--- 2-les-variables-et-types-de-donnees/exercice-2/script.js
+++ 2-les-variables-et-types-de-donnees/exercice-2/script.js
@@ -1,5 +1,5 @@
-const motDePasse;
-const userName;
-const dateAnniversaire;
-const prixDuTicket;
-const jeSuisUneVariableTresLongueAModifierEnCamelcase;
+const motDePasse = 1234;
+const userName = 'dvauzelle';
+const dateAnniversaire = 80;
+const prixDuTicket = 25;
+const jeSuisUneVariableTresLongueAModifierEnCamelcase = 'aqbdfnbdfbodfpbndfbndfpb1654bdf6sbn46gn4fgn4';
