5179aa0dd086384cdd8741f7f1f1a542e309bde7
David Vauzelle <dvauzelle@deastanceservices.fr>
2024-03-07 14:58:06 +0100

    Modification exercice 11 du chapitre 2

--- 2-les-variables-et-types-de-donnees/exercice-11/script.js
+++ 2-les-variables-et-types-de-donnees/exercice-11/script.js
@@ -3,13 +3,13 @@ let prixHT = parseInt(prompt("Quel est le prix HT ?"));
 
 // Résultat attendu dans la console, pour un prixHT de 100€ : 
 // prix HT : 100 €
-console.log(`prix HT : ${prixHT}`);
+console.log(`prix HT : ${prixHT} €`);
 
 // Pour calculer le montant de la taxe
 let tauxDeTaxe = (prixHT * 15) / 100;
 
 // prix TTC : 115 €
 let prixTTC = prixHT + tauxDeTaxe;
-console.log(`prix TTC : ${prixTTC}`);
+console.log(`prix TTC : ${prixTTC} €`);
 
 
