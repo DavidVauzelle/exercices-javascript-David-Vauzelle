37c11cc4fe33d35c66e96fdbfd9b92f06c22720e
David Vauzelle <dvauzelle@deastanceservices.fr>
2024-03-07 14:58:31 +0100

    Ajout de l'exercice 12 du chapitre 2

---
+++ 2-les-variables-et-types-de-donnees/exercice-12/script.js
@@ -0,0 +1,12 @@
+let phrase1 = 'L\'apprentissage du JavaScript est "passionnant".';
+console.log(phrase1);
+
+let phrase2 = "L'apprentissage du JavaScript est "passionnant".";
+let phrase3 = `...`
+
+// Résultat attendu dans la console :
+
+L'apprentissage du JavaScript est "passionnant".
+L'apprentissage du JavaScript est "passionnant".
+L'apprentissage du JavaScript est "passionnant".
+*/
