1efcf764a3415764846c8ca5bc1e45da259917f2
David Vauzelle <dvauzelle@deastanceservices.fr>
2024-03-07 10:30:39 +0100

    Ajout de l'exercice 7 du chapitre 2

---
+++ 2-les-variables-et-types-de-donnees/exercice-7/script.js
@@ -0,0 +1,19 @@
+let nombre1 = prompt('Nombre 1 ?');
+let nombre2 = prompt('Nombre 2 ?');
+
+// Résultat attendu pour 10 et 2, dans la console :
+
+let produit = nombre1 * nombre2;
+console.log(produit);
+
+let difference = nombre1 - nombre2;
+console.log(difference);
+
+let somme = nombre1 + nombre2;
+console.log(somme);
+
+let quotient = nombre1 / nombre2;
+console.log(quotient);
+
+let puissance = nombre1 ** nombre2;
+console.log(puissance);
