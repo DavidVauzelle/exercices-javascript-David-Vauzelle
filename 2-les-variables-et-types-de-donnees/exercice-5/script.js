968daa3c95f2ed057598ca04cc50d56b5c7a29e2
David Vauzelle <dvauzelle@deastanceservices.fr>
2024-03-07 10:01:06 +0100

    Ajout de l'exercice 5 du chapitre 2

---
+++ 2-les-variables-et-types-de-donnees/exercice-5/script.js
@@ -0,0 +1,12 @@
+let age = 25;
+let genre = "femme";
+let estContent = true;
+
+// La variable "age" est de type : number
+console.log('La variable "genre" est de type : ' + typeof age);
+
+// La variable "genre" est de type : string
+console.log('La variable "genre" est de type : ' + typeof genre);
+
+// La variable "estContent" est de type : boolean
+console.log('La variable "estContent" est de type : ' + typeof estContent);
