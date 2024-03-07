f8be9af24993c28d5e33403c4eccdb1efb6e9461
David Vauzelle <dvauzelle@deastanceservices.fr>
2024-03-07 13:22:48 +0100

    Ajout de l'exercice 10 du chapitre 2

---
+++ 2-les-variables-et-types-de-donnees/exercice-10/script.js
@@ -0,0 +1,10 @@
+let note1 = 12;
+let note2 = 15;
+let note3 = 8;
+let note4 = 5;
+
+let moyenneNote = (note1 + note2 + note3 + note4) / 4;
+
+// Résultat attendu dans la console : 
+console.log(`moyenne : ${moyenneNote}`);
+
