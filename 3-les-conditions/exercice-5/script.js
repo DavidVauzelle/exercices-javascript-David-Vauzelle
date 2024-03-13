93801aa724fec681254a1f2290f2c30aec74d8fe
David Vauzelle <dvauzelle@deastanceservices.fr>
2024-03-13 11:16:34 +0100

    Ajout de l'exercice 5 du chapitre 3

---
+++ 3-les-conditions/exercice-5/script.js
@@ -0,0 +1,22 @@
+// Demandez la note d'un élève avec un prompt() et affichez sa mention.
+
+let note = prompt('Quel est votre note ?');
+if (note < 12) {
+    // pour une note < 12
+    alert('Pas de mention');
+} else if (note >= 12 && note < 14) {
+	// Pour 12 et +
+    alert('Passable');   
+} else if (note >= 14 && note < 16) {
+	// // Pour 12 et +
+    alert('Bien');   
+} else if (note >= 16 && note < 18) {
+	// Pour 16 et +
+    alert('Très bien');   
+} else if (note >= 18 && note < 20) {
+	// Pour 18 et +
+    alert('Excellent');   
+} else {
+	// Pour 20
+    alert('Extraordinaire');   
+}
