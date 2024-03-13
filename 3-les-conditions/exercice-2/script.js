40c0a6349cc610a9a1fbd0ebbd89dcb116061d0a
David Vauzelle <dvauzelle@deastanceservices.fr>
2024-03-13 10:16:23 +0100

    Ajout de l'exercice 2 du chapitre 3

---
+++ 3-les-conditions/exercice-2/script.js
@@ -0,0 +1,22 @@
+/* Demandez deux nombres à l'utilisateur avec prompt() et affichez lequel est le plus grand, le plus petit, ou s'ils sont égaux.
+   Exemples de résultats attendus dans la console :
+        Le nombre 1 est plus grand que le nombre 2
+        Le nombre 1 est plus petit que le nombre 2
+        Les deux nombres sont égaux
+*/
+
+let number1 = prompt('Nombre 1 ?');
+let number2 = prompt('Nombre 2 ?');
+
+if (number1 > number2) {
+    // Le nombre 1 est plus grand que le nombre 2
+    console.log('Le nombre 1 est plus grand que le nombre 2');
+} else if (number1 < number2) {
+    // Le nombre 1 est plus petit que le nombre 2
+    console.log('Le nombre 1 est plus petit que le nombre 2');
+}   else {
+    console.log('Les deux nombres sont égaux'); 
+}
+
+
+
