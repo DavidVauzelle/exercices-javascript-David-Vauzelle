75e9ea5848d8e0962975318b82edc25b51ea11ff
David Vauzelle <dvauzelle@deastanceservices.fr>
2024-03-13 10:07:11 +0100

    Ajout de l'exercice 1 du chapitre 3

---
+++ 3-les-conditions/exercice-1/script.js
@@ -0,0 +1,11 @@
+// Demandez à l'utilisateur son âge avec prompt() et affichez si l'utilisateur est mineur ou majeur.
+let age = prompt('Quel est votre age ?');
+
+// Résultat attendu dans la console, si l'âge < 18 : Vous êtes mineur.
+// Résultat attendu dans la console, si l'âge >= 18 : Vous êtes majeur.
+
+if (age < 18) {
+    console.log('Vous êtes mineur.');
+} else {
+    console.log('Vous êtes majeur.');
+}
