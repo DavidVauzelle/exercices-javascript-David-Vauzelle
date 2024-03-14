8e82c5cd19cc05f141847c59c57e2b153f8d59e7
David Vauzelle <dvauzelle@deastanceservices.fr>
2024-03-14 09:54:25 +0100

    Ajout de l'exercice 1 du chapitre 4

---
+++ 4-les-boucles/script.js
@@ -0,0 +1,10 @@
+// Créez un compteur qui s'incrémente de 0 à 10, en affichant chaque nombre dans la console.
+
+let compteur = 0;
+
+// Résultat attendu dans la console :
+while (compteur <= 10) {
+    console.log(compteur);
+    compteur++;
+}
+
