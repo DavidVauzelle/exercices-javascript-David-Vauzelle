1847934ac61c040e288720a002f667939b9c3cbd
David Vauzelle <dvauzelle@deastanceservices.fr>
2024-03-14 10:51:33 +0100

    Ajout de l'exercice 3 du chapitre 4

---
+++ 4-les-boucles/exercice-3/script.js
@@ -0,0 +1,12 @@
+// Réalisez un compteur descendant de 5 à 0. 
+// Pour chaque nombre, affichez le dans la console, et à la fin, affichez "Décollage".
+
+let compteur = 5;
+
+// Résultat attendu dans la console :
+
+while (compteur > 0) {
+    console.log(compteur);
+    compteur--;
+}
+console.log("Décollage");
