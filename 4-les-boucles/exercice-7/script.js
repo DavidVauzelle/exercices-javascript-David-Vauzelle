62839c2cee67a35439c8943b694959c43febcd95
David Vauzelle <dvauzelle@deastanceservices.fr>
2024-03-14 14:21:18 +0100

    Ajout de l'exercice 7 du chapitre 4

---
+++ 4-les-boucles/exercice-7/script.js
@@ -0,0 +1,8 @@
+// Utilisez une boucle for pour afficher la table de multiplication par 7, de 1 à 10. 
+// Pour chaque multiplication, affichez le résultat dans la console.
+
+// Résultat attendu dans la console :
+let table = 7;
+for (let i = 1; i <= 10; i++) {
+    console.log(`${table} * ${i} = ${7 * i}`);
+}
